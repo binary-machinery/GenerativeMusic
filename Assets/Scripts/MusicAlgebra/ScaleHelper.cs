3619a74c74389cb236e3e0c839ffe9d1414cc9db
Fck-r-sns <fck.r.sns@gmail.com>
2019-08-20 21:35:14 +0300

    Scales

---
+++ Assets/Scripts/MusicAlgebra/ScaleHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace MusicAlgebra
+{
+    public static class ScaleHelper
+    {
+        public enum ScaleType
+        {
+            Major, // Ionian
+            Dorian,
+            Phrygian,
+            Lydian,
+            Mixolydian,
+            Minor, // Aeolian
+            Locrian
+        }
+
+        private static readonly Dictionary<ScaleType, int[]> PATTERNS = new Dictionary<ScaleType, int[]>()
+        {
+            { ScaleType.Major, new[] { 2, 2, 1, 2, 2, 2, 1 } },
+            { ScaleType.Dorian, new[] { 2, 1, 2, 2, 2, 1, 2 } },
+            { ScaleType.Phrygian, new[] { 1, 2, 2, 2, 1, 2, 2 } },
+            { ScaleType.Lydian, new[] { 2, 2, 2, 1, 2, 2, 1 } },
+            { ScaleType.Mixolydian, new[] { 2, 2, 1, 2, 2, 1, 2 } },
+            { ScaleType.Minor, new[] { 2, 1, 2, 2, 1, 2, 2 } },
+            { ScaleType.Locrian, new[] { 1, 2, 2, 1, 2, 2, 1 } },
+        };
+
+        public static Pitch[] Create(Pitch rootPitch, ScaleType scaleType)
+        {
+            int[] pattern = PATTERNS[scaleType];
+            Pitch[] scale = new Pitch[pattern.Length + 1];
+            scale[0] = rootPitch;
+            for (int i = 0; i < pattern.Length; ++i)
+            {
+                scale[i + 1] = Operators.addSemitones(scale[i], pattern[i]);
+            }
+            return scale;
+        }
+    }
+}
---
+++ Assets/Scripts/MusicAlgebra/Tests/Scales.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using MusicAlgebra;
+
+namespace Tests
+{
+    public class Scales
+    {
+        private static readonly Pitch[] C_MAJOR =
+        {
+            new Pitch(Note.C, 4),
+            new Pitch(Note.D, 4),
+            new Pitch(Note.E, 4),
+            new Pitch(Note.F, 4),
+            new Pitch(Note.G, 4),
+            new Pitch(Note.A, 4),
+            new Pitch(Note.B, 4),
+            new Pitch(Note.C, 5),
+        };
+        
+        private static readonly Pitch[] F_SHARP_MINOR =
+        {
+            new Pitch(Note.Fsharp, 4),
+            new Pitch(Note.Gsharp, 4),
+            new Pitch(Note.A, 4),
+            new Pitch(Note.B, 4),
+            new Pitch(Note.Csharp, 5),
+            new Pitch(Note.D, 5),
+            new Pitch(Note.E, 5),
+            new Pitch(Note.Fsharp, 5),
+        };
+
+        [Test]
+        public void CreateCMajor()
+        {
+            Pitch[] actual = ScaleHelper.Create(new Pitch(Note.C, 4), ScaleHelper.ScaleType.Major);
+            Assert.AreEqual(C_MAJOR, actual);
+        }
+        
+        [Test]
+        public void CreateFSharpMinor()
+        {
+            Pitch[] actual = ScaleHelper.Create(new Pitch(Note.Fsharp, 4), ScaleHelper.ScaleType.Minor);
+            Assert.AreEqual(F_SHARP_MINOR, actual);
+        }
+    }
+}
