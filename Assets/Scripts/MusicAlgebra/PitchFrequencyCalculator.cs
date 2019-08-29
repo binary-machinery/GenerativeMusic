e0ac8adc67ee74e3b07b10d2dc4732d3d44cd556
Fck-r-sns <fck.r.sns@gmail.com>
2019-08-29 22:00:28 +0300

    PitchFrequencyCalculator

---
+++ Assets/Scripts/MusicAlgebra/PitchFrequencyCalculator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace MusicAlgebra
+{
+    public class PitchFrequencyCalculator
+    {
+        private static readonly Pitch REF_PITCH = new Pitch(Note.A, 4);
+        private static readonly float REF_FREQUENCY = 440f;
+        private static readonly float FREQ_COEF = Mathf.Pow(2f, 1f / 12f);
+
+        public static float GetFrequency(Pitch pitch)
+        {
+            return REF_FREQUENCY * Mathf.Pow(FREQ_COEF, Operators.GetSemitonesDifference(pitch, REF_PITCH));
+        }
+    }
+}
