cc0d9bffdca29e6c0d2f13f2449735561e231faa
Evgeniy Prikhodko <fck.r.sns@gmail.com>
2019-09-04 21:39:56 +0300

    Player uses chords

--- Assets/Scripts/SoundPlayer.cs
+++ Assets/Scripts/SoundPlayer.cs
@@ -9,12 +9,13 @@ public class SoundPlayer : MonoBehaviour
     [SerializeField]
     private AbstractInstrument _instrument;
 
-    private AcademicScale _academicScale;
+    private AcademicChord[] _academicChords;
     private Queue<Pitch> _queue;
 
     private void Awake()
     {
-        _academicScale = ScaleHelper.Create(Note.C, ScaleType.Major);
+        AcademicScale scale = ScaleHelper.Create(Note.C, ScaleType.Major);
+        _academicChords = ScaleHelper.GenerateAcademicChords(scale);
         _queue = new Queue<Pitch>();
     }
 
@@ -27,19 +28,19 @@ public class SoundPlayer : MonoBehaviour
     {
         if (_queue.Count == 0)
         {
-            Pitch nextPitch = GetNextPitch();
-            _queue.Enqueue(nextPitch);
-            _queue.Enqueue(Operators.AddSemitones(nextPitch, 3));
-            _queue.Enqueue(Operators.AddSemitones(nextPitch, 7));
+            AcademicChord academicChord = GetNextChord();
+            _queue.Enqueue(new Pitch(academicChord.notes[0], 4));
+            _queue.Enqueue(new Pitch(academicChord.notes[1], 4));
+            _queue.Enqueue(new Pitch(academicChord.notes[2], 4));
         }
         float volume = beatEvent.isStrong ? 1f : 0.75f;
         Pitch pitch = _queue.Dequeue();
         _instrument.PlayNote(pitch, volume);
     }
 
-    private Pitch GetNextPitch()
+    private AcademicChord GetNextChord()
     {
-        int index = Random.Range(0, _academicScale.notes.Length);
-        return new Pitch(_academicScale.notes[index], 4);
+        int index = Random.Range(0, _academicChords.Length);
+        return _academicChords[index];
     }
 }
