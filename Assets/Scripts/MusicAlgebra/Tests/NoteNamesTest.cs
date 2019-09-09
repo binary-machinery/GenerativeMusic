8843b66d3ad866ceac72617ad12cbf90e068fb36
Evgeniy Prikhodko <fck.r.sns@gmail.com>
2019-09-09 21:47:32 +0300

    Tests renamed

--- Assets/Scripts/MusicAlgebra/Tests/NoteNamesTest.cs
+++ Assets/Scripts/MusicAlgebra/Tests/NoteNamesTest.cs
@@ -6,73 +6,73 @@ namespace Tests
     public class NoteNamesTest
     {
         [Test]
-        public void testNameForA()
+        public void TestNameForA()
         {
             Assert.AreEqual("A", NoteNames.Get(Note.A));
         }
 
         [Test]
-        public void testNameForAsharp()
+        public void TestNameForAsharp()
         {
             Assert.AreEqual("A#", NoteNames.Get(Note.Asharp));
         }
 
         [Test]
-        public void testNameForB()
+        public void TestNameForB()
         {
             Assert.AreEqual("B", NoteNames.Get(Note.B));
         }
 
         [Test]
-        public void testNameForC()
+        public void TestNameForC()
         {
             Assert.AreEqual("C", NoteNames.Get(Note.C));
         }
 
         [Test]
-        public void testNameForCsharp()
+        public void TestNameForCsharp()
         {
             Assert.AreEqual("C#", NoteNames.Get(Note.Csharp));
         }
 
         [Test]
-        public void testNameForD()
+        public void TestNameForD()
         {
             Assert.AreEqual("D", NoteNames.Get(Note.D));
         }
 
         [Test]
-        public void testNameForDsharp()
+        public void TestNameForDsharp()
         {
             Assert.AreEqual("D#", NoteNames.Get(Note.Dsharp));
         }
 
         [Test]
-        public void testNameForE()
+        public void TestNameForE()
         {
             Assert.AreEqual("E", NoteNames.Get(Note.E));
         }
 
         [Test]
-        public void testNameForF()
+        public void TestNameForF()
         {
             Assert.AreEqual("F", NoteNames.Get(Note.F));
         }
 
         [Test]
-        public void testNameForFsharp()
+        public void TestNameForFsharp()
         {
             Assert.AreEqual("F#", NoteNames.Get(Note.Fsharp));
         }
 
         [Test]
-        public void testNameForG()
+        public void TestNameForG()
         {
             Assert.AreEqual("G", NoteNames.Get(Note.G));
         }
 
         [Test]
-        public void testNameForGsharp()
+        public void TestNameForGsharp()
         {
             Assert.AreEqual("G#", NoteNames.Get(Note.Gsharp));
         }
