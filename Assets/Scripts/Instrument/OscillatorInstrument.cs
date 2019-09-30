b11222423951b09ad733ea4507bdfc4cc5d41bff
Evgeniy Prikhodko <fck.r.sns@gmail.com>
2019-09-30 21:45:43 +0300

    Master gain

--- Assets/Scripts/Instrument/OscillatorInstrument.cs
+++ Assets/Scripts/Instrument/OscillatorInstrument.cs
@@ -5,12 +5,16 @@ public class OscillatorInstrument : AbstractInstrument
 {
     [SerializeField]
     private Oscillator _oscillatorPrefab;
+
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float _masterGain = 0.5f;
     
     public override AbstractSoundController PlayNote(Pitch pitch, float volume, int durationQuarterBeats)
     {
         Oscillator oscillator = Instantiate(_oscillatorPrefab, gameObject.transform);
         oscillator.frequency = PitchFrequencyCalculator.GetFrequency(pitch);
-        oscillator.gain = volume;
+        oscillator.gain = volume * _masterGain;
         return new OscillatorSoundController(oscillator, durationQuarterBeats);
     }
 }
