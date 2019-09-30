b9e790b65df4690de6e70efa42f63ad419cee551
Evgeniy Prikhodko <fck.r.sns@gmail.com>
2019-09-30 21:46:24 +0300

    Visualization

--- Assets/Scripts/Visualization/VisualizationManager.cs
+++ Assets/Scripts/Visualization/VisualizationManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using MusicAlgebra;
 using UnityEngine;
 
 namespace Visualization
@@ -6,7 +7,9 @@ namespace Visualization
     public class VisualizationManager : MonoBehaviour
     {
         private const float Z_DEPTH = 5;
-        private const int PLAYED_SOUNDS_TIME_TO_LIVE = 12; // in quarter beats 
+        private const int PLAYED_SOUNDS_TIME_TO_LIVE = 24; // in quarter beats
+        private const int NOTE_GRID_OFFSET = -4;
+        private const int OCTAVE_GRID_OFFSET = -4;
 
         [SerializeField]
         private BeatManager _beatManager;
@@ -24,11 +27,14 @@ namespace Visualization
         private float _pitchStep;
 
         private Dictionary<int, Sound> _sounds;
+        private HashSet<int> _soundsToRemove;
         private Transform _soundsParent;
 
         private void Awake()
         {
             _sounds = new Dictionary<int, Sound>();
+            _soundsToRemove = new HashSet<int>();
+
             _beatManager.onQuarterBeatEvent += OnQuarterBeatEvent;
             _queue.onSoundAdded += OnSoundAdded;
 
@@ -41,17 +47,38 @@ namespace Visualization
             foreach (var kv in _sounds)
             {
                 Sound sound = kv.Value;
+                if (!sound.gameObject.activeSelf)
+                {
+                    sound.gameObject.SetActive(true);
+                }
+
                 int beatGridIndex = sound.playableSound.startQuarterBeatNumber - quarterBeatEvent.quarterBeatNumber;
                 float x = beatGridIndex * _quarterBeatStep;
-                int noteGridIndex = (int)sound.playableSound.pitch.note;
+
+                Pitch pitch = sound.playableSound.pitch;
+                int noteGridIndex = (int)pitch.note + NOTE_GRID_OFFSET + (pitch.octave + OCTAVE_GRID_OFFSET) * Defines.SEMITONES_COUNT;
                 float y = noteGridIndex * _pitchStep;
+
                 sound.transform.localPosition = new Vector3(x, y, Z_DEPTH);
+
+                if (beatGridIndex + sound.playableSound.durationQuarterBeats < -PLAYED_SOUNDS_TIME_TO_LIVE)
+                {
+                    _soundsToRemove.Add(sound.playableSound.id);
+                }
             }
+
+            foreach (int id in _soundsToRemove)
+            {
+                Destroy(_sounds[id].gameObject);
+                _sounds.Remove(id);
+            }
+            _soundsToRemove.Clear();
         }
 
         private void OnSoundAdded(PlayableSound playableSound)
         {
             Sound sound = Instantiate(_soundPrefab, _soundsParent);
+            sound.gameObject.SetActive(false);
             sound.playableSound = playableSound;
             _sounds[playableSound.id] = sound;
         }
