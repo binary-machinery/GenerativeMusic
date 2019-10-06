1d51d4ea2fa9c07bdeea91a7956733b94088eaa3
Evgeniy Prikhodko <fck.r.sns@gmail.com>
2019-10-06 19:14:56 +0300

    Composer detached from SoundPlayer

---
+++ Assets/Scripts/Composer.cs
@@ -0,0 +1,44 @@
+﻿using MusicAlgebra;
+using UnityEngine;
+
+public class Composer : MonoBehaviour
+{
+    [SerializeField]
+    private BeatManager _beatManager;
+
+    [SerializeField]
+    private PlayableSoundQueue _queue;
+    
+    private AcademicChord[] _academicChords;
+
+    private void Awake()
+    {
+        AcademicScale scale = ScaleHelper.Create(Note.C, ScaleType.Major);
+        _academicChords = ScaleHelper.GenerateAcademicChords(scale);
+        
+        _beatManager.onBeatEvent += OnBeatEvent;
+    }
+
+    private void OnBeatEvent(BeatEvent beatEvent)
+    {
+        while (_queue.count < 20)
+        {
+            AcademicChord academicChord = GetRandomChord();
+            PlayableSound lastSound = _queue.GetLastSound();
+            int quarterBeatNumber = beatEvent.quarterBeatNumber;
+            if (lastSound != null)
+            {
+                quarterBeatNumber = lastSound.startQuarterBeatNumber + lastSound.durationQuarterBeats;
+            }
+            _queue.AddSound(new PlayableSound(new Pitch(academicChord.notes[0], 4), 1f, quarterBeatNumber, 4));
+            _queue.AddSound(new PlayableSound(new Pitch(academicChord.notes[1], 4), 1f, quarterBeatNumber, 4));
+            _queue.AddSound(new PlayableSound(new Pitch(academicChord.notes[2], 4), 1f, quarterBeatNumber, 4));
+        }
+    }
+    
+    private AcademicChord GetRandomChord()
+    {
+        int index = Random.Range(0, _academicChords.Length);
+        return _academicChords[index];
+    }
+}
--- Assets/Scripts/SoundPlayer.cs
+++ Assets/Scripts/SoundPlayer.cs
@@ -13,15 +13,12 @@ public class SoundPlayer : MonoBehaviour
     [SerializeField]
     private PlayableSoundQueue _queue;
 
-    private AcademicChord[] _academicChords;
     private Dictionary<int, AbstractSoundController> _soundControllers;
     private HashSet<int> _soundsToStop;
     private int _soundCounter;
 
     private void Awake()
     {
-        AcademicScale scale = ScaleHelper.Create(Note.C, ScaleType.Major);
-        _academicChords = ScaleHelper.GenerateAcademicChords(scale);
         _soundControllers = new Dictionary<int, AbstractSoundController>();
         _soundsToStop = new HashSet<int>();
     }
@@ -54,20 +51,6 @@ public class SoundPlayer : MonoBehaviour
 
     private void OnBeatEvent(BeatEvent beatEvent)
     {
-        while (_queue.count < 20)
-        {
-            AcademicChord academicChord = GetNextChord();
-            PlayableSound lastSound = _queue.GetLastSound();
-            int quarterBeatNumber = beatEvent.quarterBeatNumber;
-            if (lastSound != null)
-            {
-                quarterBeatNumber = lastSound.startQuarterBeatNumber + lastSound.durationQuarterBeats;
-            }
-            _queue.AddSound(new PlayableSound(new Pitch(academicChord.notes[0], 4), 1f, quarterBeatNumber, 4));
-            _queue.AddSound(new PlayableSound(new Pitch(academicChord.notes[1], 4), 1f, quarterBeatNumber, 4));
-            _queue.AddSound(new PlayableSound(new Pitch(academicChord.notes[2], 4), 1f, quarterBeatNumber, 4));
-        }
-
         PlayableSound playableSound;
         while ((playableSound = _queue.GetNextForQuarterBeat(beatEvent.quarterBeatNumber)) != null)
         {
@@ -75,11 +58,5 @@ public class SoundPlayer : MonoBehaviour
                 playableSound.pitch, playableSound.volume, playableSound.durationQuarterBeats);
             _soundControllers[_soundCounter++] = soundController;
         }
-    }
-
-    private AcademicChord GetNextChord()
-    {
-        int index = Random.Range(0, _academicChords.Length);
-        return _academicChords[index];
     }
 }
